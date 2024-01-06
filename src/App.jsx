fadcc7fee98dca00b37d92bbac210dde1ae89edd
unknown <karthikeya3762@gmail.com>
2024-01-06 21:26:06 +0530

    React task intial cleaning setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Create from "./components/Create";
+import Read from "./components/Read";
+import Update from "./components/Update";
+
+
+
+
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="main">
+      <p className="position-sticky">
+        <span>
+          <a href="/create" className="btn btn-primary me-3">
+            HOME
+          </a>
+        </span>
+        <span>
+          <a href="/read" className="btn btn-primary">
+            Update/Delete
+          </a>
+        </span>
+      </p>
+      <h1 className="mt-2 CrudContent">Crud Operation</h1>
+
+      <Router>
+        <Routes>
+          <Route path="/create" element={<Create />} />
+
+          <Route id="/read"  path="/read" element={<Read />} />
+
+          <Route  path="/update" element={<Update />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Read.jsx
@@ -0,0 +1,79 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+
+function Read() {
+  const [apidata, setApidatat] = useState([]);
+  const navigate = useNavigate();
+
+  const updateData = ({ id, firstname, lastName, email }) => {
+    localStorage.setItem("id", id);
+    localStorage.setItem("firstname", firstname);
+    localStorage.setItem("lastName", lastName);
+    localStorage.setItem("email", email);
+    navigate("/update");
+  };
+
+  const deleteData = async (id) => {
+    await axios.delete(`https://659908a6a20d3dc41cef2cb9.mockapi.io/users/${id}`);
+    callApi();
+  };
+
+  const callApi = async () => {
+    try {
+      const res = await axios.get("https://659908a6a20d3dc41cef2cb9.mockapi.io/users");
+      setApidatat(res.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  return (
+    <>
+      <div className="container mt-4">
+        <div className="text-center">Read, Update, Delete and Operation</div>
+        <div className="table-responsive">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Roll No</th>
+                <th scope="col">FirstName</th>
+                <th scope="col">LastName</th>
+                <th scope="col">Email</th>
+                <th scope="col">Delete</th>
+                <th scope="col">Update</th>
+              </tr>
+            </thead>
+            <tbody>
+              {apidata.map((data) => (
+                <tr key={data.id}>
+                  <th scope="row">{data.id}</th>
+                  <td>{data.firstname}</td>
+                  <td>{data.lastName}</td>
+                  <td>{data.email}</td>
+                  <td>
+                    <button onClick={() => deleteData(data.id)} className="btn btn-danger">
+                      Delete
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={() => updateData(data)} className="btn btn-info">
+                      Update
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Read;
